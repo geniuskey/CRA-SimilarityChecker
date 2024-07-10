8e5145deeab0dd42531c2014e9d10346a7076a0c
geniuskey <geniuskey@gmail.com>
2024-07-10 16:12:17 +0900

    알파벳 비교

--- game.py
+++ game.py
@@ -1,3 +1,22 @@
 class Game:
     def run(self):
         pass
+
+    def check_alpha(self, a, b) -> int:
+        a_map = [False] * 26
+        b_map = [False] * 26
+        for c in a:
+            index = ord(c) - ord('A')
+            a_map[index] = True
+
+        for c in b:
+            index = ord(c) - ord('A')
+            b_map[index] = True
+
+        total_count, same_count = 0, 0
+        for i in range(26):
+            if a_map[i] or b_map[i]:
+                total_count += 1
+            if a_map[i] and b_map[i]:
+                same_count += 1
+        return int((same_count/total_count) * 40)
--- test_game.py
+++ test_game.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
 
+from game import Game
+
 
 class TestGame(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.game = Game()
+
     def test_run(self):
         self.fail()
+
+    def test_check_alpha(self):
+        test_cases = [
+            ("ASD", "DSA", 40),
+            ("A", "BB", 0),
+            ("AAABB", "BAA", 40),
+            ("AA", "AAE", 20),
+        ]
+        for a, b, ans in test_cases:
+            with self.subTest(f"{a}, {b}, {ans}"):
+                ret = self.game.check_alpha(a, b)
+                self.assertEqual(ret, ans)
